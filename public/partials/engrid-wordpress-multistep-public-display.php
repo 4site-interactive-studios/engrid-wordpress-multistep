7aed92e7c21bd8ba76dcc5db5c78238d695d2e3d
Fernando Santos <fern@ndo.io>
2022-04-08 13:09:25 -0300

    ACF Update

--- public/partials/engrid-wordpress-multistep-public-display.php
+++ public/partials/engrid-wordpress-multistep-public-display.php
@@ -11,6 +11,3 @@
  * @package    Engrid_Wordpress_Multistep
  * @subpackage Engrid_Wordpress_Multistep/public/partials
  */
-?>
-
-<!-- This file should primarily consist of HTML with a little bit of PHP. -->
